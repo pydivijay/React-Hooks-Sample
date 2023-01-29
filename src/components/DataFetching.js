9425239a1616af53950fee4291e792221ac4cab7
pydivijay <vijaykumar519.pydi@gmail.com>
2023-01-29 22:03:25 +0530

    Fetching data

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -3,22 +3,33 @@ import axios from "axios";
 
 function DataFetching() {
   const [posts, setPosts] = useState([]);
+  const [id, setId] = useState(1);
+  const [idFromButtonClick, setIdFromClick] = useState(1);
+  const handleClick = () => {
+    setIdFromClick(id);
+  };
+
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts")
+      .get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
       .then((res) => {
         setPosts(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }, [idFromButtonClick]);
   return (
     <div>
-      {posts &&
+      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <button type="button" onClick={handleClick}>
+        FetchData
+      </button>
+      <div>{posts.title}</div>
+      {/* {posts &&
         posts.map((post) => (
           <li key={post.id}>{post.title}</li>
-        ))}
+        ))} */}
     </div>
   );
 }
