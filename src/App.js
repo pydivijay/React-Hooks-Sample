4d5f085189e621a6408cf54cf4db74a6aebeafd5
pydivijay <vijaykumar519.pydi@gmail.com>
2023-02-08 11:50:09 +0530

    useContext and useReduer Hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import './App.css';
-import ClassCounterone from './components/ClassCounterone';
-import ClassMouse from './components/ClassMouse';
-import HookCounterOne from './components/HookCounterOne';
-import HookMouse from './components/HookMouse';
-import HookUsers from './components/HookUsers';
-import MouseContainer from './components/MouseContainer';
-import HookAxiosExample from './components/HookAxiosExample';
-import EmployeeData from './components/EmployeeData';
-import EmployeeSaveData from './components/EmployeeSaveData';
-import IntervalClassCounter from './components/IntervalClassCounter';
-import IntervalHookCounter from './components/IntervalHookCounter';
-import DataFetching from './components/DataFetching';
+import "./App.css";
+import ClassCounterone from "./components/ClassCounterone";
+import ClassMouse from "./components/ClassMouse";
+import HookCounterOne from "./components/HookCounterOne";
+import HookMouse from "./components/HookMouse";
+import HookUsers from "./components/HookUsers";
+import MouseContainer from "./components/MouseContainer";
+import HookAxiosExample from "./components/HookAxiosExample";
+import EmployeeData from "./components/EmployeeData";
+import EmployeeSaveData from "./components/EmployeeSaveData";
+import IntervalClassCounter from "./components/IntervalClassCounter";
+import IntervalHookCounter from "./components/IntervalHookCounter";
+import DataFetching from "./components/DataFetching";
+import DataObjectChange from "./components/DataObjectChange";
+import ComponentC from "./components/ComponentC";
+
+import React from "react";
+import CounterOne from "./components/CounterOne";
+
+export const UserContext = React.createContext();
+
+export const ChannelContext = React.createContext();
+
 function App() {
   return (
     <div className="App">
@@ -30,7 +40,14 @@ function App() {
       {/* <EmployeeSaveData/> */}
       {/* <IntervalClassCounter/>
       <IntervalHookCounter/> */}
-      <DataFetching/>
+      {/* <DataFetching/> */}
+      {/* <DataObjectChange/> */}
+      {/* <UserContext.Provider value={"vijay"}>
+        <ChannelContext.Provider value={'vijay test'}>
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider> */}
+      <CounterOne/>
     </div>
   );
 }
